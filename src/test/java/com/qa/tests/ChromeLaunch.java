cb671e79d10d4571bdfa052582943ae9dc09fe47
Shiv Pandarge <shiv.pandarge@gmail.com>
2020-10-01 23:42:12 +0530

    added wait

--- src/test/java/com/qa/tests/ChromeLaunch.java
+++ src/test/java/com/qa/tests/ChromeLaunch.java
@@ -3,6 +3,7 @@ import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;  
 import org.openqa.selenium.chrome.ChromeDriver;  
 import org.testng.annotations.Test;
+import java.util.concurrent.TimeUnit;
   
 public class ChromeLaunch {  
 	
