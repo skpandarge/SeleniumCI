df938a80b02acad4153c592f6d40e7a007145f1c
Shiv Pandarge <shiv.pandarge@gmail.com>
2020-10-01 18:15:08 +0530

    chromelaunch script added

--- src/test/java/com/qa/tests/ChromeLaunch.java
+++ src/test/java/com/qa/tests/ChromeLaunch.java
@@ -2,6 +2,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;  
 import org.openqa.selenium.WebDriver;  
 import org.openqa.selenium.chrome.ChromeDriver;  
+import org.testng.annotations.Test;
   
 public class ChromeLaunch {  
 	@Test
